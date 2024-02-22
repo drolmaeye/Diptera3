33c53da35519659705200c8d37d8a3205894a741
jssmith <hp.jssmith@gmail.com>
2024-02-22 15:59:13 -0600

    up and running

--- Tutorials1.py
+++ Tutorials1.py
@@ -1,1 +1,13 @@
-print('hello world')
+from epics import Motor
+
+m = Motor('16test:m2')
+
+# a = m.show_info()
+
+b = m.move(0, relative=False, wait=True)
+
+# print(m.within_limits(10))
+
+print(b)
+print(m.readback)
+
